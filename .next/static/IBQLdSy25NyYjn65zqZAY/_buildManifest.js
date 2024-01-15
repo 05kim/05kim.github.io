599e8ae486410078b718539925cb490cc4524980
05kim <yhkim123@fastcampus.co.kr>
2024-01-15 21:27:26 +0900

    build gitignore 제거

---
+++ .next/static/IBQLdSy25NyYjn65zqZAY/_buildManifest.js
@@ -0,0 +1,33 @@
+;(self.__BUILD_MANIFEST = (function (s, a, e) {
+  return {
+    __rewrites: { beforeFiles: [], afterFiles: [], fallback: [] },
+    '/': ['static/chunks/pages/index-da0a167bb4822bff.js'],
+    '/404': ['static/chunks/pages/404-16480b14923a2348.js'],
+    '/_error': ['static/chunks/pages/_error-a9f36ce656a10e9d.js'],
+    '/about': [s, a, e, 'static/chunks/pages/about-0edcfeaf91e5f645.js'],
+    '/blog': ['static/chunks/pages/blog-9746294794488363.js'],
+    '/blog/page/[page]': ['static/chunks/pages/blog/page/[page]-db65ad5975219495.js'],
+    '/blog/[...slug]': [s, a, e, 'static/chunks/pages/blog/[...slug]-a30cfc3f8c9a3c53.js'],
+    '/projects': [s, 'static/chunks/pages/projects-d332d62a74b815d3.js'],
+    '/tags': ['static/chunks/pages/tags-ace8dd89695ad9c0.js'],
+    '/tags/[tag]': ['static/chunks/pages/tags/[tag]-e241dcfe3df3f717.js'],
+    sortedPages: [
+      '/',
+      '/404',
+      '/_app',
+      '/_error',
+      '/about',
+      '/blog',
+      '/blog/page/[page]',
+      '/blog/[...slug]',
+      '/projects',
+      '/tags',
+      '/tags/[tag]',
+    ],
+  }
+})(
+  'static/chunks/675-64a821d6ab4fd557.js',
+  'static/chunks/146-f253c6b6eaa0adbd.js',
+  'static/chunks/712-5e55e3547fb5a534.js'
+)),
+  self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
